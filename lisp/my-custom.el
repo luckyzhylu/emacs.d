eac863a137e17bda61d9bbc9a0b6c36f279ffb51
zhangyunlu <luckyzhylu@gmail.com>
2018-11-05 18:14:03 +0800

    V1.1

---
+++ lisp/my-custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (flycheck-ycmd flycheck abc-mode avy company-anaconda popup pythonic hydra pallet ivy-hydra move-text helm-ag goto-chg powerline which-key flx-ido ido-vertical-mode auto-highlight-symbol auto-yasnippet company-c-headers ggtags python-mode highlight-symbol smex dsvn google-c-style undo-tree anaconda-mode smartparens use-package ace-jump-mode highlight-parentheses yasnippet yasnippet-snippets company-shell cask undo-tree monokai-theme company helm-projectile ivy counsel swiper ace-window magit aggressive-indent hungry-delete ggtags exec-path-from-shell rainbow-delimiters fill-column-indicator browse-kill-ring bm helm-cscope xcscope yatemplate popwin window-numbering expand-region clang-format))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
